e402fde5f763b24c9b9e9a1198367c8782654c03
mwai <mwaijohng@gmail.com>
2020-07-14 15:45:34 +0300

    word counter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div id="footer">
-          2020
+          &copy; 2020
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -17,37 +17,66 @@ class Home extends Component {
         let word = e.target.value
         // word = word.replace(/↵/g, "hghgh");
         //split the text to get number of words
+
+        word = word.replace(/(^\s*)|(\s*$)/gi, "");//exclude  start and end white-space
+        word = word.replace(/[ ]{2,}/gi, " ");//2 or more space to 1
+        word = word.replace(/\n /, "\n"); // exclude newline with a start spacing
+
         const wordArray = word.split(" ")//returns an array of words
-        const wordArrayCount = wordArray.length
+        let wordArrayCount = word.split(' ').filter(function (str) { return str != ""; }).length;
+        wordArrayCount = word.trim().split(/\s+/).length
+
+        try{
+            //wordArrayCount = word.match(/\b\S+\b/g).length
+        }catch{
+
+        }
         
-        console.log(wordArray)
+       // console.log(wordArray)
         this.setState({
             count: wordArrayCount,
             words: word,
-            wordArray: wordArray
+            wordsArray: wordArray
         })
     }
 
     keywordDensity = (e) => {
-        let keyword = e.target.value
+        let keyword = e.target.value.trim()
 
-        let wordArray = this.state.wordsArray
-        let count = 0;
+        let words = this.state.words
+        //let count = 0;
+        //var count = (wordArray.match(/df/g) || []).length;
+
+        let count = 0
+        let subStr = keyword.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+        if (keyword.length <= 0) {
+            count = words.length + 1;
+        }else{
+            console.log(subStr)
+            count = words.match(new RegExp(subStr, 'gi') || []).length
+            console.log(count)
+            console.log(words)
+        }
+        
+
         //console.log(wordArray[2])
-        wordArray.forEach(element => {
-            console.log(element)
-
-            if (element.includes(keyword)) {
-                //console.log(element)
-                count += 1
-            }
-        });
+        // wordArray.forEach(element => {
+        //     console.log(element)
+        //     //replace spaces in element
+        //     let newElement = element.replace(" ","")
+        //     let newKeyword = keyword.replace(" ","")
+        //     if (newElement.includes(newKeyword)) {
+        //         //console.log(element)
+        //         count += 1
+        //     }
+        // });
 
         //calculate density
-        let density = (count / wordArray.length) * 100
+        let density = (count / this.state.count) * 100
         this.setState({
             density: density
         })
+    
     }
 
     render() {
