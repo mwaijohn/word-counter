11f2e49947f3f763da516f91657bd2a449397eeb
mwai <mwaijohng@gmail.com>
2020-07-15 16:02:41 +0300

    Updates

---
+++ precache-manifest.a4619dfe5acdce89d128131240005277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd76e64e208d6ea849c9c3f74b89bb51",
+    "url": "/word-counter/index.html"
+  },
+  {
+    "revision": "b56ebfd21edd486be729",
+    "url": "/word-counter/static/css/main.c9b854c3.chunk.css"
+  },
+  {
+    "revision": "522881c2bb8cf6c84bf0",
+    "url": "/word-counter/static/js/2.2c4a05dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-counter/static/js/2.2c4a05dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56ebfd21edd486be729",
+    "url": "/word-counter/static/js/main.5f82d5c4.chunk.js"
+  },
+  {
+    "revision": "0829d83fe2cbce3030d5",
+    "url": "/word-counter/static/js/runtime-main.613b9faa.js"
+  }
+]);
